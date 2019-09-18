e1c4649b7cc94ad4ebc0e9c94ab7701273b5e5b7
Roger Oliveira <rogeroliveira84@gmail.com>
2019-09-18 17:14:17 +1000

    =Initial commit

---
+++ src/script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    if ("IntersectionObserver" in window) {
+
+        var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+        let lazyImageObserver = new IntersectionObserver((entries, observer) => {
+
+            entries.forEach(entry => {
+
+                if (entry.isIntersecting) {
+                    console.log('intersected...')
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.classList.remove("lazy");
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+
+        lazyImages.forEach(lazyImage => {
+            lazyImageObserver.observe(lazyImage);
+        });
+
+    } else {
+
+        // Intersection Observer is not support, so load the images straightaway
+        var lazyImages = document.getElementsByClassName("lazy");
+        [].forEach.call(lazyImages, function (lazyImage) {
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.classList.remove("lazy");
+            lazyImage.height = 'auto';
+        });
+    }
+
+});
